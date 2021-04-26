576c652cf8d36950d9c75a51871369e7e0086282
Mubarak <12859241+mcmubby@users.noreply.github.com>
2021-04-26 12:57:26 +0100

    end of array day 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace Arrays
 {
@@ -6,7 +7,37 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // string [] students = new string [4];
+            // students[0] = "elijah";
+            // students[1] = "okechukwu";
+            // students[2] = "bernice";
+            // students[3] = "ekene";
+
+            // Console.WriteLine(students[2]);
+
+            string [] students = {"elijah","okechukwu","bernice","ekene"};
+            int [] evenNumbers = {2,4,6,8};
+
+            // foreach (var i in evenNumbers)
+            // {
+            //     Console.WriteLine(i+2);
+            // }
+
+            // for (int i = 0; i < 4; i+=2)
+            // {
+            //     Console.WriteLine(students[i]);
+            // }
+            // Console.WriteLine(Array.Find(students, name => name.StartsWith("ber")));
+           // Console.WriteLine(Array.IndexOf(students, "bernice"));
+
+            // int [,] a = new int [3,4] {{0,1,2,3}, {4,5,6,7}, {8,9,10,11}};
+            // for (int i = 0; i < 3; i++)
+            // {
+            //     for (int j = 0; j < 4; j++)
+            //     {
+            //         Console.WriteLine(a[i,j]);
+            //     }
+            // }
         }
     }
 }
