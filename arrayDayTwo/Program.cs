97cb79cfdd69dbb8943c6a391a37bc3b20d62f83
Mubarak <12859241+mcmubby@users.noreply.github.com>
2021-04-27 12:48:31 +0100

    Arrays day 2

---
+++ arrayDayTwo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace arrayDayTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string [] students = {"elijah", "bryan", "micheal", "matt", "muiz"};
+
+            // // Console.WriteLine(Array.FindIndex(students, 0, 4, (e => e.Contains("bry"))));
+
+            // int [] strangeNumbers = {89, 34, 79, 5, 10, 43};
+
+            // // Array.Sort(strangeNumbers);
+            // // Array.Reverse(strangeNumbers);
+            // // foreach (var i in strangeNumbers)
+            // // {
+            // //     Console.WriteLine(i);
+            // // }
+
+            // Array.Clear(strangeNumbers,0,2);
+            // Console.WriteLine(strangeNumbers[0]+" "+" "+strangeNumbers[1]+" "+strangeNumbers[2]);
+
+            int [] givenArray = {23,47,89,64,102,90,2,5,10};
+            Array.Reverse(givenArray);
+            int arraySum = 0;
+            foreach (var i in givenArray)
+            {
+                arraySum+=i;
+            }
+            Console.WriteLine(arraySum);
+            Console.WriteLine(givenArray.Sum());
+
+        }
+    }
+}
